6ad2c6701a7d28b723c934c7a7e2b3767cf80b8f
Tim DiSabatino <tim@pathwaysdigital.com>
2017-03-01 06:48:43 -0800

    Loops & iterators exercises

---
+++ loops_iterators/each_with_index.rb
@@ -0,0 +1,6 @@
+cars = ['BMW', 'Mercedes-Benz', 'Honda', 'Subaru', 'Ford']
+
+cars.each_with_index do |item, index|
+  puts "#{item} is in position #{index}"
+end
+
---
+++ loops_iterators/stop_loop.rb
@@ -0,0 +1,8 @@
+x = ""
+while x != "STOP" do
+  puts "Hi, how is everything?"
+  ans = gets.chomp
+  puts "Would you like to keep going? Type STOP to exit."
+  x = gets.chomp
+end
+
