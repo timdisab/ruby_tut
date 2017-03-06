105f6a6e3916390817fa136d3cb3581ad82237a7
Tim DiSabatino <tim@pathwaysdigital.com>
2017-03-06 10:43:48 -0800

    Add more stuff exercises

---
+++ more_stuff/regex.rb
@@ -0,0 +1,16 @@
+# boolean_regex.rb
+
+def has_lab?(string)
+  if string =~ /[Ll]ab/
+    puts string
+  else
+    puts "No match here."
+  end
+end
+
+
+has_lab?("laboratory")
+has_lab?("experiment")
+has_lab?("Pans Labyrinth")
+has_lab?("elaborate")
+has_lab?("polar bear")
